b97213787b8e80aaae4b6d2efe2bd7b921b6ae32
pedromdcr <suspectmachine@gmail.com>
2023-12-01 11:40:23 -0300

    targets and colision with projectiles implemented

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ import sys
 import math
 from bullet import Bullet
 from square import Square
+from targets import Targets
 
 pygame.init()
 
 WIDTH, HEIGHT = 800, 600
 FPS = 60
 WHITE = (255, 255, 255)
-BLUE = (0, 0, 255)
+BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 
 player = Square(WIDTH, HEIGHT)
 bullet = Bullet()
+targets = Targets()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Clique no Quadrado")
 clock = pygame.time.Clock()
+
+
+def check_collision(rect1, rect2):
+    return rect1.colliderect(rect2)
+
 
 while True:
     pressing = False
@@ -29,6 +36,10 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
                 bullet.shoot(player.x, player.y, player.size, player.angle)
+            if event.key == pygame.K_x:
+                targets.randomize_position(10)
+
+    targets.move_targets_to_center(WIDTH // 2, HEIGHT // 2, targets.speed)
 
     keys = pygame.key.get_pressed()
 
@@ -37,16 +48,29 @@ while True:
     if keys[pygame.K_LEFT]:
         player.rotation("left")
 
-    for projectile in bullet.list:
+    for projectile in bullet.list[:]:
         projectile[0] += bullet.speed * math.cos(math.radians(projectile[2]))
         projectile[1] += bullet.speed * math.sin(math.radians(projectile[2]))
 
-    screen.fill(WHITE)
+        bullet_rect = pygame.Rect(projectile[0], projectile[1], bullet.size, bullet.size)
 
-    for projectile in bullet.list:
-        pygame.draw.rect(screen, BLUE, (projectile[0], projectile[1], bullet.size, bullet.size))
+        for target in targets.targets[:]:
+            target_rect = pygame.Rect(*target[0], target[1], target[1])
+            if check_collision(bullet_rect, target_rect):
+                # Se houver colisão, remove o projétil e o alvo
+                bullet.list.remove(projectile)
+                targets.targets.remove(target)
+                break
 
-    player.draw_rotated_square(screen, RED, player.x, player.y, player.angle)
+    screen.fill(BLACK)
+
+    for projectile in bullet.list[:]:
+        pygame.draw.rect(screen, WHITE, (projectile[0], projectile[1], bullet.size, bullet.size))
+
+    for target in targets.targets[:]:
+        targets.create(screen, WHITE)
+
+    player.draw_rotated_square(screen, WHITE, player.x, player.y, player.angle)
 
     pygame.display.flip()
 
---
+++ targets.py
@@ -0,0 +1,43 @@
+import random
+import math
+import pygame
+
+
+class Targets:
+    def __init__(self):
+        self.speed = 1
+        self. cord_x = None
+        self.cord_y = None
+
+        self.targets = []
+
+    def randomize_position(self, number):
+        for number in range(number):
+            x = random.randint(-400, 1200)
+            if x < 0 or x > 800:
+                self.cord_x = random.choice([-100, 900])
+                self.cord_y = random.randint(0, 600)
+                size = random.randint(15, 55)
+                self.targets.append([[self.cord_x, self.cord_y], size])
+
+            elif 0 <= x <= 800:
+                self.cord_x = x
+                self.cord_y = random.choice([-100, 700])
+                size = random.randint(15, 55)
+                self.targets.append([[self.cord_x, self.cord_y], size])
+
+        print(self.targets)
+
+    def create(self, screen, color):
+        for target in self.targets:
+            pygame.draw.rect(screen, color, (*target[0], target[1], target[1]))
+
+    def move_targets_to_center(self, center_x, center_y, target_speed):
+        for target in self.targets:
+            target_x, target_y = target[0]
+            angle = math.atan2(center_y - target_y, center_x - target_x)
+            target[0][0] += target_speed * math.cos(angle)
+            target[0][1] += target_speed * math.sin(angle)
+
+
+target1 = Targets()
